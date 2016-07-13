5281988fef9a5187e64be846f8e844ce5e3fd3bb
mod <moddum_35505@hotmail.com>
2016-07-13 11:12:31 +0700

    check Choose

--- app/src/main/java/rus/aimthiang/thipphawan/rusrun/SignUpActivity.java
+++ app/src/main/java/rus/aimthiang/thipphawan/rusrun/SignUpActivity.java
@@ -14,7 +14,7 @@ public class SignUpActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton avata0RadioButton,avata1RadioButton ,avata2RadioButton,avata3RadioButton,
             avata4RadioButton;
-
+    private String nameString, userString, passwordString,avataString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +33,71 @@ public class SignUpActivity extends AppCompatActivity {
         avata0RadioButton = (RadioButton) findViewById(R.id.radioButton4);
         avata0RadioButton = (RadioButton) findViewById(R.id.radioButton5);
 
+        //Radio Controller
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switch (checkedId) {
+
+                    case R.id.radioButton:
+                        avataString = "0";
+                        break;
+                    case R.id.radioButton2:
+                        avataString = "1";
+                        break;
+                    case R.id.radioButton3:
+                        avataString = "2";
+                        break;
+                    case R.id.radioButton4:
+                        avataString ="3";
+                        break;
+                    case R.id.radioButton5:
+                        avataString ="4";
+                        break;
+
+
+                } // switch
+            }
+        });
+
     }// Main Aethod
 
     public  void clickSignUpSign(View view) {
 
+       //Get Value from Edit Text
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง","กรุณากรอกทุกช่อง คะ");
+
+        } else if (checkChoose()){
+            //Checked
+
+        } else {
+           //Un Check
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "ยังไม่เลือก Avata",
+                    "กรุณาเลือก Avata ด้วยคะ");
+        }
+
+
     } // ClickSignUp
+
+    private boolean checkChoose() {
+        boolean status = true;
+        status = avata0RadioButton.isChecked() ||
+                avata1RadioButton.isChecked() ||
+                avata2RadioButton.isChecked() ||
+                avata3RadioButton.isChecked() ||
+                avata4RadioButton.isChecked() ;
+    }
 
 
 }  // Main Clss
